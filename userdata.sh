7b6aadb8f94c77c016c5613a893dcfc97335b93f
dharaniaws <158130556+dharaniaws@users.noreply.github.com>
2024-05-23 19:31:13 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,13 +1,7 @@
-#!/bin/bash 
-sudo yum install -y git
-sudo yum install -y docker 
-sudo systemctl start docker
-sudo systemctl enable docker 
-sudo usermod -aG docker ec2-user 
-sudo chmod 666 /var/run/docker.sock 
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
-sudo chmod +x /usr/local/bin/docker-compose 
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
-git clone https://github.com/VKK07/wordpress.git 
-cd wordpress/
-docker-compose up -d
+sudo yum update
+sudo yum install git -y
+git clone https://github.com/dharaniaws/USA-Housing.git
+cd USA-Housing/
+sudo yum install python3-pip -y
+pip install -r requirements.txt
+screen -m -d python3 app.py
