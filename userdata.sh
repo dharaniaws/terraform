2fa483d7cd7dc25b4e0bca8775e282079bd2c507
dharaniaws <158130556+dharaniaws@users.noreply.github.com>
2024-05-24 20:58:41 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -6,3 +6,9 @@ cd USA-Housing/
 sudo yum install python3 python3-pip -y
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+terraform init
+terraform plan
+terraform apply -auto-approve
