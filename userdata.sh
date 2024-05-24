20806bd58e33b072783ba5df508bd501179a7dd4
dharaniaws <158130556+dharaniaws@users.noreply.github.com>
2024-05-24 18:10:13 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo yum install git -y
-git clone https://github.com/USA-Housing.git
+sudo git clone https://github.com/dharaniaws/USA-Housing
 cd /
 cd USA-Housing/
 sudo yum install python3 python3-pip -y
 pip3 install -r requirements.txt
-python3 app.py
+screen -m -d python3 app.py
